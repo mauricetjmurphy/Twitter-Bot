00371ca5ccbb908af9fc9bc41579d8cec89116bb
root <root@DESKTOP-VP3SBSV.localdomain>
2021-10-12 21:13:12 +0100

    Updated the selenium bot

--- seleniumBot.py
+++ seleniumBot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
@@ -15,6 +16,7 @@ load_dotenv(dotenv_path=env_path)
 
 
 driver = webdriver.Firefox(executable_path=r'env/bin/geckodriver.exe')
+driver.maximize_window()
 
 
 user = os.environ.get("TWITTER_USERNAME")
@@ -27,30 +29,26 @@ def login():
     print( driver.title )
     time.sleep(5)
     inputElement1 = driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input")
-    time.sleep(2)
+    time.sleep(1)
     inputElement1.send_keys(user)
-    time.sleep(2)
+    time.sleep(1)
     button1 = driver.find_element_by_xpath('/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div')
     button1.click()
-    time.sleep(2)
+    time.sleep(1)
     inputElement2 = driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/label/div/div[2]/div/input")
-    time.sleep(2)
+    time.sleep(1)
     inputElement2.send_keys(password)
-    time.sleep(2)
+    time.sleep(1)
     button2 = driver.find_element_by_xpath('/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div')
     button2.click()
     time.sleep(5)
 
-    try:
-        WebDriverWait(driver, 10).until(EC.title_contains("Home"))
-        print( driver.title )
-    finally:
-        pass
+    
 
-def stats(screen_name):
+def stats():
     following = "0"
     followers = "0"
-    driver.get(f"https://twitter.com/{screen_name}")
+    
     time.sleep(5)
     data1 = driver.find_element_by_xpath("/html/body/div")
     m1 = re.search(data1.text, "\n(\d+) Following\n(\d)+Followers\n")
@@ -82,7 +80,47 @@ def search(search_term):
     print(users_who_posted)
 
 
-def followUsers(search_term):
+
+def followVisible():
+    run = True
+    while run:
+        count = 0
+        time.sleep(2)
+        arr = getFollowButtons()
+        print(f'There are {len(arr)} left to follow on this page')
+        for i,el in enumerate(arr):
+            try:
+                print(f'Click {i}')
+                el.click()
+                count += 1
+                time.sleep(1)
+            except ElementClickInterceptedException as e:
+                print(e)
+                print('Overload! Sleeping for 15 minutes...')
+                time.sleep(900)
+                driver.refresh()
+                time.sleep(3)
+                break
+            except Exception as e:
+                print(e)
+                break
+        if len(arr) > 30:
+            run = False
+        elif len(arr) == 0:
+            print('Scrolling...')
+            scrollPage()
+            time.sleep(2)
+        else:
+            run = True
+     
+        
+
+def scrollPage():
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    time.sleep(3)
+
+
+def navTopFollowersPage(search_term):
     driver.get("https://twitter.com/search?q=" + search_term + "&src=typed_query")
     time.sleep(5)
 
@@ -94,24 +132,34 @@ def followUsers(search_term):
     followersLink.click()
     time.sleep(3)
 
-    followersContainer = driver.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/section")
-    followButtons = driver.find_elements_by_xpath("//*[contains(text(), 'Follow')]")
+    cookiesCloseBtn = driver.find_element_by_xpath('/html/body/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/span/span')
+    cookiesCloseBtn.click()
+    time.sleep(2)
 
-    for el in followButtons:
-        el.click()
-    
-    print(followButtons)
-    
-    # buttonContainers = followersContainer.find_element_by_tag_name('span')
-    # for el in buttonContainers:
-    #     button = el.find
+def getFollowButtons():
+    followButtons = driver.find_elements_by_xpath("//span[text()='Follow']")
+    return followButtons
+
+   
+
 
 
 login()
-followUsers('rarible')
+
+try:
+    WebDriverWait(driver, 10).until(EC.title_contains("Home"))
+    print('Successful Login!')
+finally:
+    pass
+
+navTopFollowersPage('rarible')
+
+for i in range(0, 8):
+    print(f'Starting session {i+1} of 8')
+    followVisible()
+    scrollPage()
+    
 
 
-
-time.sleep(1000)
 driver.quit()
 
